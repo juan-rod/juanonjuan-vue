4c55e244aeff427e163a783ce7ab615ec4d1b33f
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2020-02-10 21:25:48 -0600

    started on wireframes

--- src/components/wireframes/WireframeOne.vue
+++ src/components/wireframes/WireframeOne.vue
@@ -1,6 +1,40 @@
 <template>
-  <div class="wireframes-home--one">
-    ONE
+  <div class="wireframes-home--one border-red">
+    <nav class="wireframes-home--one-nav border-green">
+      <div class="nav-elements border-yellow one-nav-logo">f</div>
+      <div class="nav-elements border-yellow one-nav-sound-off">sound off</div>
+      <div class="nav-elements border-yellow one-nav-social">
+        <p class="border-pink">facebook</p>
+        <p class="border-pink">soundcloud</p>
+        <p class="border-pink">youtube</p>
+        <p class="border-pink">instagram</p>
+      </div>
+      <div class="nav-elements border-yellow one-nav-hamburger">=</div>
+    </nav>
+    <main class="wireframes-home--one-main border-green">
+      <div class="one-main-side-bar border-yellow">
+        <div class="top-bar border-pink">
+          <div class="border-orangered top-bar--title">
+            <p class="border-blue">pianist,</p>
+            <p class="border-blue">composer,</p>
+            <p class="border-blue">sound maker:</p>
+          </div>
+          <div class="border-orangered top-bar--links">
+            <a class="border-blue" href="">News</a>
+            <a class="border-blue" href="">Events</a>
+          </div>
+        </div>
+        <div class="bottom-bar border-pink">
+          <p class="border-orangered">Paulo</p>
+          <p class="border-orangered">Forenso</p>
+          <span class="border-orangered">
+            <a class="border-blue" href="">Contact Me</a>
+            <a class="border-blue" href="">Info</a>
+          </span>
+        </div>
+      </div>
+      <div class="one-main-main-bar border-yellow"></div>
+    </main>
   </div>
 </template>
 
